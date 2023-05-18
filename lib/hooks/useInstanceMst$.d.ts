0dd10ee568b96ae86ff73d858901f17fee6f640d
igor.efrem <igor.efrem@carasent.com>
2023-05-18 12:37:40 +0200

    build

--- lib/hooks/useInstanceMst$.d.ts
+++ lib/hooks/useInstanceMst$.d.ts
@@ -1,37 +1,4 @@
 import type { IStateTreeNode } from 'mobx-state-tree';
-/**
- * @important
- *      initFn.name must be unique please do not use anonymous functions or .create() from MST but rather wrap it in a new function with a unique name
- *
- * @var do_not_persist ?bool should not be persisted to indexedDB default false
- * @var unique_index required string if you need to have multiple instances of the same store with different data
- * @var initFn should return the MST store
- *
- * @example
- *
- *      function createAnonymousSchemaRoot$() {
- *          #optionally if you need to to have some predefined data in snapshot
- *          const optionalSnapshot = {...}
- *
- *          const store = AnonymousSchemaRoot$.create(optionalSnapshot)
- *          //write your logic for creating the store here
- *
- *          //optional applySnapshot if you want to do an async request and then apply to the snapshot
- *          fetchDataFromSomewhere()
- *              .then((data) => { applySnapshot(store, data)})
- *              .catch((err) => {
- *                  //do something logic here to reset data etc
- *                  console.error(err)
- *              })
- *
- *          return store
- *      }
- *
- *      const AnonymousSchemaIndex: React.FC = () => {
- *          const anonymous_schema$ = useInitMst$(createAnonymousSchemaRoot$)
- *      ...
- *      }
- */
 export declare function useInstanceMst$<T extends IStateTreeNode, IFDB extends (store: object) => {
     unregisterAll: () => void;
     idb_check_promise: Promise<void>;
