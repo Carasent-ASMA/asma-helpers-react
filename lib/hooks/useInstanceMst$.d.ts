cfcae3b0fc07cfea45067ad3f029a7ef1d87cbae
igor.efrem <igor.efrem@carasent.com>
2023-06-02 19:47:32 +0200

    v0.1.7

--- lib/hooks/useInstanceMst$.d.ts
+++ lib/hooks/useInstanceMst$.d.ts
@@ -1,11 +1,15 @@
 import type { IStateTreeNode } from 'mobx-state-tree';
+import type MakeInspectable from 'mobx-devtools-mst';
 export declare function useInstanceMst$<T extends IStateTreeNode, IFDB extends (store: object) => {
     unregisterAll: () => void;
     idb_check_promise: Promise<void>;
-}>(initFn: () => T, initIDBListenersOnMstSn: IFDB, { unique_index, do_not_persist, inspectable, }: {
+}>(initFn: () => T, initIDBListenersOnMstSn: IFDB, { unique_index, do_not_persist, inspectable, mobxDevtoolsMst, }: {
     unique_index: string;
     do_not_persist?: boolean;
     inspectable: boolean;
+    mobxDevtoolsMst: () => Promise<{
+        default: typeof MakeInspectable;
+    }>;
 }): T;
 export default useInstanceMst$;
 //# sourceMappingURL=useInstanceMst$.d.ts.map
