ac8b030c96b043df21318c825cf9633fe76681c5
Sebastian Darie <sebastian.darie@carasent.com>
2026-05-13 11:10:21 +0300

    fix(exports): ASMA-7163 Export new hooks from index

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,4 +2,6 @@ export * from './helpers/CreateInjectableContext.js';
 export * from './helpers/lazyWithRetries.js';
 export * from './hooks/_useInstanceMst$.js';
 export * from './hooks/_useHistoryListen.js';
+export * from './hooks/useHideParam.js';
+export * from './hooks/useParam.js';
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './helpers/lazyWithRetries.js'
 
 export * from './hooks/_useInstanceMst$.js'
 export * from './hooks/_useHistoryListen.js'
+export * from './hooks/useHideParam.js'
+export * from './hooks/useParam.js'
